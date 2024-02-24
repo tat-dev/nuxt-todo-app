603b14888b3a0a12e25eecd8887464dc66dd10bd
hellcat <broum226.i@gmail.com>
2024-02-24 11:26:08 +0900

    cardのクリックイベント修正

--- components/todo/card.vue
+++ components/todo/card.vue
@@ -1,21 +1,20 @@
 <template>
-  <v-card class="my-3">
+  <v-card class="my-3" @click="isOpenDialog = true">
     <template v-if="isEdit">
       <v-card-text>
-        <!-- <todo-form is-edit :value="inputVal" @update="update($event)" @blur="isInput = false"></todo-form> -->
         <v-text-field label="TODOを編集" variant="underlined" :autofocus="isEdit" v-model="todo.name" ref="input" @keyup.enter="$event.target.blur()" @blur="update"></v-text-field>
       </v-card-text>
     </template>
     <template v-else>
       <v-card-title>
-        <span class="cursor-pointer" @click="isOpenDialog = true">{{ todo.name }}</span>
+        <span>{{ todo.name }}</span>
       </v-card-title>
     </template>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn icon @click="$emit('done', todo.id!)"><v-icon icon="mdi-check" color="green-darken-2" variant="text" /></v-btn>
-      <v-btn icon @click="isEdit = !isEdit"><v-icon icon="mdi-pencil" variant="text" /></v-btn>
-      <v-btn icon @click="$emit('delete', todo.id!)"><v-icon icon="mdi-delete" variant="text" /></v-btn>
+      <v-btn icon @click.stop="$emit('done', todo.id!)"><v-icon icon="mdi-check" color="green-darken-2" variant="text" /></v-btn>
+      <v-btn icon @click.stop="isEdit = !isEdit"><v-icon icon="mdi-pencil" variant="text" /></v-btn>
+      <v-btn icon @click.stop="$emit('delete', todo.id!)"><v-icon icon="mdi-delete" variant="text" /></v-btn>
     </v-card-actions>
   </v-card>
   <todo-detail-dialog v-model="isOpenDialog"></todo-detail-dialog>
